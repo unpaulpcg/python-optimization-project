6c449fe71a092e4c8f695e6ffce4ff33c60fbb2b
unpaulpcg <unpaulpcg@gmail.com>
2026-06-21 09:12:07 +0900

    Delete src/after directory

--- src/after/config.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass(frozen=True)
-class RunConfig:
-    learning_rate: float = 0.001
-    epochs: int = 3
-    batch_size: int = 1000
-    top_k_words: int = 20
